b3fcafc17c00644db1d29959f2cbfab6f44a48a6
Marius POPOVICI <mpopovici@clj.bitdefender.biz>
2019-04-30 20:36:11 +0300

    20:36

--- Assign2/assign2/furnitures/urls.py
+++ Assign2/assign2/furnitures/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.furniture_list),
+    url(r'^$', views.furniture_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.furniture_detail, name="detail"),
 ]
--- Assign2/assign2/furnitures/views.py
+++ Assign2/assign2/furnitures/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Furniture
+from django.http import HttpResponse
 # Create your views here.
 
 def furniture_list(request):
     furnitures = Furniture.objects.all().order_by('name')
     return render(request, 'furnitures/furniture_list.html', {'furnitures': furnitures})
+
+def furniture_detail(request, slug):
+    return HttpResponse(slug)
