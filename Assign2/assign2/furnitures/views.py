cbe72f646df25df53a629e387f678e0daa1d2d00
Marius POPOVICI <mpopovici@clj.bitdefender.biz>
2019-05-02 02:21:35 +0300

    02:21

--- Assign2/assign2/furnitures/views.py
+++ Assign2/assign2/furnitures/views.py
@@ -7,7 +7,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def furniture_list(request):
-    furnitures = Furniture.objects.all().order_by('name')
+    if request.method == 'POST':
+        if request.POST.get('filter') == "sale_type":
+            furnitures = Furniture.objects.all().order_by('saleType')
+        elif request.POST.get('filter') == "price":
+            furnitures = Furniture.objects.all().order_by('newPrice')
+        else:
+            furnitures = Furniture.objects.all().order_by('name')
+    else:
+        furnitures = Furniture.objects.all().order_by('name')
     return render(request, 'furnitures/furniture_list.html', {'furnitures': furnitures})
 
 def furniture_detail(request, slug):
--- Assign2/assign2/orders/models.py
+++ Assign2/assign2/orders/models.py
@@ -11,6 +11,6 @@ class Order(models.Model):
     client = models.ForeignKey(User, default=None)
     furniture = models.ForeignKey(furnitureModels.Furniture, default=None)
     status = models.CharField(max_length=50)
-    # date - to be added
+    date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return str(self.id)
